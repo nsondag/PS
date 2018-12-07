a7e0b3ab800072ff0697b728fd9604217fad6df7
Nadja Sondag <nsondag@w-r2-p3.s19.be>
2018-12-07 15:47:59 +0100

    adding tests

---
+++ test_500.sh
@@ -0,0 +1,200 @@
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
+ARG=`ruby -e "puts (0..499).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG" | eval "./checker $ARG"
+./push_swap $ARG | wc -l
---
+++ test_mix.sh
@@ -0,0 +1,200 @@
+ARG1=`ruby -e "puts (-15..84).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG1" | eval "./checker $ARG1"
+./push_swap $ARG1 | wc -l
+ARG2=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG2" | eval "./checker $ARG2"
+./push_swap $ARG2 | wc -l
+ARG3=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG3" | eval "./checker $ARG3"
+./push_swap $ARG3 | wc -l
+ARG4=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG4" | eval "./checker $ARG4"
+./push_swap $ARG4 | wc -l
+ARG5=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG5" | eval "./checker $ARG5"
+./push_swap $ARG5 | wc -l
+ARG6=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG6" | eval "./checker $ARG6"
+./push_swap $ARG6 | wc -l
+ARG7=`ruby -e "puts (-199..-100).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG7" | eval "./checker $ARG7"
+./push_swap $ARG7 | wc -l
+ARG8=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG8" | eval "./checker $ARG8"
+./push_swap $ARG8 | wc -l
+ARG9=`ruby -e "puts (-99..0).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG9" | eval "./checker $ARG9"
+./push_swap $ARG9 | wc -l
+ARG10=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG10" | eval "./checker $ARG10"
+./push_swap $ARG10 | wc -l
+ARG11=`ruby -e "puts (-50..49).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG11" | eval "./checker $ARG11"
+./push_swap $ARG11 | wc -l
+ARG12=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG12" | eval "./checker $ARG12"
+./push_swap $ARG12 | wc -l
+ARG13=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG13" | eval "./checker $ARG13"
+./push_swap $ARG13 | wc -l
+ARG14=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG14" | eval "./checker $ARG14"
+./push_swap $ARG14 | wc -l
+ARG15=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG15" | eval "./checker $ARG15"
+./push_swap $ARG15 | wc -l
+ARG16=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG16" | eval "./checker $ARG16"
+./push_swap $ARG16 | wc -l
+ARG17=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG17" | eval "./checker $ARG17"
+./push_swap $ARG17 | wc -l
+ARG18=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG18" | eval "./checker $ARG18"
+./push_swap $ARG18 | wc -l
+ARG19=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG19" | eval "./checker $ARG19"
+./push_swap $ARG19 | wc -l
+ARG20=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG20" | eval "./checker $ARG20"
+./push_swap $ARG20 | wc -l
+ARG21=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG21" | eval "./checker $ARG21"
+./push_swap $ARG21 | wc -l
+ARG22=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG22" | eval "./checker $ARG22"
+./push_swap $ARG22 | wc -l
+ARG23=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG23" | eval "./checker $ARG23"
+./push_swap $ARG23 | wc -l
+ARG24=`ruby -e "puts (1000..1099).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG24" | eval "./checker $ARG24"
+./push_swap $ARG24 | wc -l
+ARG25=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG25" | eval "./checker $ARG25"
+./push_swap $ARG25 | wc -l
+ARG26=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG26" | eval "./checker $ARG26"
+./push_swap $ARG26 | wc -l
+ARG27=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG27" | eval "./checker $ARG27"
+./push_swap $ARG27 | wc -l
+ARG28=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG28" | eval "./checker $ARG28"
+./push_swap $ARG28 | wc -l
+ARG29=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG29" | eval "./checker $ARG29"
+./push_swap $ARG29 | wc -l
+ARG30=`ruby -e "puts (-99..0).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG30" | eval "./checker $ARG30"
+./push_swap $ARG30 | wc -l
+ARG31=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG31" | eval "./checker $ARG31"
+./push_swap $ARG31 | wc -l
+ARG32=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG32" | eval "./checker $ARG32"
+./push_swap $ARG32 | wc -l
+ARG33=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG33" | eval "./checker $ARG33"
+./push_swap $ARG33 | wc -l
+ARG34=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG34" | eval "./checker $ARG34"
+./push_swap $ARG34 | wc -l
+ARG35=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG35" | eval "./checker $ARG35"
+./push_swap $ARG35 | wc -l
+ARG36=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG36" | eval "./checker $ARG36"
+./push_swap $ARG36 | wc -l
+ARG37=`ruby -e "puts (-1..98).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG37" | eval "./checker $ARG37"
+./push_swap $ARG37 | wc -l
+ARG38=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG38" | eval "./checker $ARG38"
+./push_swap $ARG38 | wc -l
+ARG39=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG39" | eval "./checker $ARG39"
+./push_swap $ARG39 | wc -l
+ARG40=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG40" | eval "./checker $ARG40"
+./push_swap $ARG40 | wc -l
+ARG41=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG41" | eval "./checker $ARG41"
+./push_swap $ARG41 | wc -l
+ARG42=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG42" | eval "./checker $ARG42"
+./push_swap $ARG42 | wc -l
+ARG43=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG43" | eval "./checker $ARG43"
+./push_swap $ARG43 | wc -l
+ARG44=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG44" | eval "./checker $ARG44"
+./push_swap $ARG44 | wc -l
+ARG45=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG45" | eval "./checker $ARG45"
+./push_swap $ARG45 | wc -l
+ARG46=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG46" | eval "./checker $ARG46"
+./push_swap $ARG46 | wc -l
+ARG47=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG47" | eval "./checker $ARG47"
+./push_swap $ARG47 | wc -l
+ARG48=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG48" | eval "./checker $ARG48"
+./push_swap $ARG48 | wc -l
+ARG49=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG49" | eval "./checker $ARG49"
+./push_swap $ARG49 | wc -l
+ARG50=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG50" | eval "./checker $ARG50"
+./push_swap $ARG50 | wc -l
+ARG51=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG51" | eval "./checker $ARG51"
+./push_swap $ARG51 | wc -l
+ARG52=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG52" | eval "./checker $ARG52"
+./push_swap $ARG52 | wc -l
+ARG53=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG53" | eval "./checker $ARG53"
+./push_swap $ARG53 | wc -l
+ARG54=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG54" | eval "./checker $ARG54"
+./push_swap $ARG54 | wc -l
+ARG55=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG55" | eval "./checker $ARG55"
+./push_swap $ARG55 | wc -l
+ARG56=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG56" | eval "./checker $ARG56"
+./push_swap $ARG56 | wc -l
+ARG57=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG57" | eval "./checker $ARG57"
+./push_swap $ARG57 | wc -l
+ARG58=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG58" | eval "./checker $ARG58"
+./push_swap $ARG58 | wc -l
+ARG59=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG59" | eval "./checker $ARG59"
+./push_swap $ARG59 | wc -l
+ARG60=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG60" | eval "./checker $ARG60"
+./push_swap $ARG60 | wc -l
+ARG61=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG61" | eval "./checker $ARG61"
+./push_swap $ARG61 | wc -l
+ARG62=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG62" | eval "./checker $ARG62"
+./push_swap $ARG62 | wc -l
+ARG63=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG63" | eval "./checker $ARG63"
+./push_swap $ARG63 | wc -l
+ARG64=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG64" | eval "./checker $ARG64"
+./push_swap $ARG64 | wc -l
+ARG65=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG65" | eval "./checker $ARG65"
+./push_swap $ARG65 | wc -l
+ARG66=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG66" | eval "./checker $ARG66"
+./push_swap $ARG66 | wc -l
+ARG67=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG67" | eval "./checker $ARG67"
+./push_swap $ARG67 | wc -l
+ARG68=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG68" | eval "./checker $ARG68"
+./push_swap $ARG68 | wc -l
+ARG69=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG69" | eval "./checker $ARG69"
+./push_swap $ARG69 | wc -l
+ARG70=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG70" | eval "./checker $ARG70"
+./push_swap $ARG70 | wc -l
+ARG71=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG71" | eval "./checker $ARG71"
+./push_swap $ARG71 | wc -l
+ARG72=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG72" | eval "./checker $ARG72"
+./push_swap $ARG72 | wc -l
+ARG73=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG73" | eval "./checker $ARG73"
+./push_swap $ARG73 | wc -l
+ARG74=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG74" | eval "./checker $ARG74"
+./push_swap $ARG74 | wc -l
+ARG75=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG75" | eval "./checker $ARG75"
+./push_swap $ARG75 | wc -l
+ARG76=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG76" | eval "./checker $ARG76"
+./push_swap $ARG76 | wc -l
+ARG77=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG77" | eval "./checker $ARG77"
+./push_swap $ARG77 | wc -l
+ARG78=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG78" | eval "./checker $ARG78"
+./push_swap $ARG78 | wc -l
+ARG79=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG79" | eval "./checker $ARG79"
+./push_swap $ARG79 | wc -l
+ARG80=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG80" | eval "./checker $ARG80"
+./push_swap $ARG80 | wc -l
+ARG81=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG81" | eval "./checker $ARG81"
+./push_swap $ARG81 | wc -l
+ARG82=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG82" | eval "./checker $ARG82"
+./push_swap $ARG82 | wc -l
+ARG83=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG83" | eval "./checker $ARG83"
+./push_swap $ARG83 | wc -l
+ARG84=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG84" | eval "./checker $ARG84"
+./push_swap $ARG84 | wc -l
+ARG85=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG85" | eval "./checker $ARG85"
+./push_swap $ARG85 | wc -l
+ARG86=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG86" | eval "./checker $ARG86"
+./push_swap $ARG86 | wc -l
+ARG87=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG87" | eval "./checker $ARG87"
+./push_swap $ARG87 | wc -l
+ARG88=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG88" | eval "./checker $ARG88"
+./push_swap $ARG88 | wc -l
+ARG89=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG89" | eval "./checker $ARG89"
+./push_swap $ARG89 | wc -l
+ARG90=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG90" | eval "./checker $ARG90"
+./push_swap $ARG90 | wc -l
+ARG91=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG91" | eval "./checker $ARG91"
+./push_swap $ARG91 | wc -l
+ARG92=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG92" | eval "./checker $ARG92"
+./push_swap $ARG92 | wc -l
+ARG93=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG93" | eval "./checker $ARG93"
+./push_swap $ARG93 | wc -l
+ARG94=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG94" | eval "./checker $ARG94"
+./push_swap $ARG94 | wc -l
+ARG95=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG95" | eval "./checker $ARG95"
+./push_swap $ARG95 | wc -l
+ARG96=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG96" | eval "./checker $ARG96"
+./push_swap $ARG96 | wc -l
+ARG97=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG97" | eval "./checker $ARG97"
+./push_swap $ARG97 | wc -l
+ARG98=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG98" | eval "./checker $ARG98"
+./push_swap $ARG98 | wc -l
+ARG99=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG99" | eval "./checker $ARG99"
+./push_swap $ARG99 | wc -l
+ARG100=`ruby -e "puts (0..99).to_a.shuffle.join(' ')"` ; eval "./push_swap $ARG100" | eval "./checker $ARG100"
+./push_swap $ARG100 | wc -l
