2ab645668dde4ab8e95542bbde1d7f6422bf6a37
Nadja Sondag <nsondag@w-r2-p3.s19.be>
2018-12-07 17:38:17 +0100

    Visualising stack a before any operation

--- visualization.c
+++ visualization.c
@@ -6,7 +6,7 @@
 /*   By: nsondag <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/05 18:15:59 by nsondag           #+#    #+#             */
-/*   Updated: 2018/12/05 20:30:44 by nsondag          ###   ########.fr       */
+/*   Updated: 2018/12/07 17:37:30 by nsondag          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,36 +17,43 @@ int				key_hook(int keycode, t_visu *v)
 	if (keycode == 53)
 	{
 		mlx_destroy_window(v->mlx_ptr, v->win_ptr);
-		//exit(0);
+		exit(0);
 	}
 	return (0);
 }
 
-void		visualization(void)
+void		visualization(t_stack a, t_stack b)
 {
 	t_visu	v;
+	int		i;
+	int		j;
+	int		k;
 
+	i = 0;
+	k = 0;
 	v.mlx_ptr = mlx_init();
 	v.win_ptr = mlx_new_window(v.mlx_ptr, WIN_WIDTH, WIN_HEIGHT, "PUSH_SWAP");
 	mlx_key_hook(v.win_ptr, key_hook, &v);
-	mlx_loop(v.mlx_ptr);	
+	while (i < a.len)
+	{
+		j = a.tab[i];
+		while (j > 0)
+		{
+			k = -1;
+			while (++k < 9) 
+				mlx_pixel_put(v.mlx_ptr, v.win_ptr,
+						10 *j + k, i * 10 + k , 0xFF3300);
+			j--;
+		}
+		i++;
+	}
+	i = 0;
+	(void)b.tab[i];
+	//{
+	//	j = b.tab[i] + 1;
+		//while (j > 0)
+		//	mlx_pixel_put(v.mlx_ptr, v.win_ptr, i, j--, 255);
+	//	i++;
+	//}
+	mlx_loop(v.mlx_ptr);
 }
-
-void		*fill_pixel(t_visu *visu, int x, int y)
-{
-	unsigned int position;
-
-	if (x >= 0 && y >= 0 && x < WIN_WIDTH && y < WIN_HEIGHT)
-		position = (x + WIN_WIDTH * y) * 4;
-	else
-		return (0);
-	visu->data[position] = 1;
-	visu->data[position + 1] = 1;
-	visu->data[position + 2] = 1;
-	return (0);
-}
-
-//void		init()
-//{
-
-//}
